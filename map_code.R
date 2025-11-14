59890217b0096a0d5e12c38de0358e0f77937d28
Anna Duan <annaduan@sas.upenn.edu>
2025-11-14 09:52:53 -0500

    map formatting

--- map_code.R
+++ map_code.R
@@ -17,8 +17,6 @@ data_center_density <- st_read("data/data_center_density.geojson")
 basemap <- st_read("data/basemap.geojson")
 
 #### Map ####
-
-
 hex_states <- data_centers_hex %>%
   group_by(state) %>%
   summarize(geometry = st_union(x))
@@ -27,42 +25,42 @@ ggplot() +
   geom_sf(data = basemap, fill = "black", color = NA, size = 0.01) + 
   geom_sf(
     data = data_centers_hex %>% filter(centers > 0),
-    aes(fill = centers),
+    aes(fill = centers/10),
     color = "gray10"
   ) +
   geom_sf(data = data_centers_hex, color = "gray10", size = 0.5) +
   geom_sf(data = basemap, fill = NA, color = "gray20") +
+  geom_text(aes(label = str_wrap("Data Center Alley, Northern Virginia", 38), x = 3179603, y = 3000), size = 4, color = "lightgoldenrod1", family = "Satoshi", fontface = "bold", hjust = 1, vjust = 1) +
+  geom_text(aes(label = str_wrap("Data Center Alley in Northern Virginia has the densest concentration of data centers in the world, with 300+ data centers and 4900 megawatts in energy capacity.", 38), x = 3179603, y = 0), size = 3, color = "lightgoldenrod3", family = "Satoshi", hjust = 1, vjust = 1) +
   labs(
-    title = "A quarter of global internet traffic passes through Virginia's Data Center Alley",
-    subtitle = "Data Center Alley in Northern Virginia has the densest concentration of data centers in the world, with 300+ data centers and 4900 megawatts in energy capacity.",
-    caption = "Source: OpenStreetMap Contributors\nAuthor: Anna Duan and Penn Urban Tech Club",
-    fill = "Data center density\n(centers per 500sqmi)"
+    title = "A qurter of global internet traffic passes through Virginia's Data Center Alley",
+    subtitle = "Log-transformed data centers per 1000 square-mile hexogon grids as of November 2025",
+    caption = "#30DayMapChallenge Day 14: OpenStreetMap\nSource: OpenStreetMap Contributors, 2025\nAnna Duan X Penn Urban Tech Club",
+    fill = "Data center density\n(Centers/100sqmi)"
   ) +
-  scale_fill_viridis_b(
+  scale_fill_viridis_c(
     option = "A",
     trans = "log",
-    n.breaks = 6,
-    limits = c(1, 130),
     labels = scales::label_number()
   ) +
   theme_void() +
   theme(
     legend.position = c(0.1, 0.2),
     plot.background = element_rect(fill = "black"),
-    
-    text = element_text(color = "goldenrod1", family = "Satoshi"),
-    
+    text = element_text(color = "orchid3", family = "Satoshi"),
     plot.title.position = "plot",
     plot.title = element_text(
       face = "bold",
-      size = 18,
-      color = "goldenrod1",
-      margin = margin(b = 5)
+      size = 21,
+      color = "lightgoldenrod1",
+      margin = margin(b = 5),
+      hjust = 0,
     ),
     
     plot.subtitle = element_text(
       size = 14,
-      color = "goldenrod2",
+      hjust = 0,
+      color = "lightgoldenrod3",
       margin = margin(b = 10)
     ),
     
@@ -70,10 +68,7 @@ ggplot() +
     plot.caption = element_text(
       size = 9,
       hjust = 0.5,
-      color = "orchid3",
       margin = margin(t = 10)
     ),
-    
-    plot.margin = margin(t = 20, r = 10, b = 20, l = 10)
+    plot.margin = margin(t = 20, r = 20, b = 20, l = 20)
   )
-
