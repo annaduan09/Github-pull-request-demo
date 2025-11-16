d46c446dfb42a6404aaaec4f96168013952efe29
Anna Duan <annaduan@sas.upenn.edu>
2025-11-16 18:09:33 -0500

    map tweaks and final jpg
    fixed typos, brightened colors

--- map_code.R
+++ map_code.R
@@ -17,25 +17,25 @@ basemap <- st_read("data/basemap.geojson")
 #### Groups 1 and 2: Modify the map ####
 ggplot() +
   geom_sf(data = basemap, fill = "black", color = "transparent", size = 0.01) + 
+  geom_sf(data = data_centers_hex, color = "gray18", size = 0.5) +
   geom_sf(
     data = data_centers_hex %>% filter(centers > 0),
     aes(fill = centers/10),
-    color = "gray12"
+    color = "gray18"
   ) +
-  geom_sf(data = data_centers_hex, color = "gray12", size = 0.5) +
-  geom_sf(data = basemap, fill = "transparent", color = "gray22") +
+  geom_sf(data = basemap, fill = "transparent", color = "gray35") +
   geom_polygon(aes(x,y),
                data = {h<-sqrt(10000)*1609.344/2; cx<-1581937; cy<-315796.5;
                data.frame(x=c(cx-h,cx+h,cx+h,cx-h),
                           y=c(cy-h,cy-h,cy+h,cy+h))},
                fill=NA, color="lightgoldenrod2") +
-  geom_text(aes(label = str_wrap("Data Center Alley, Northern Virginia", 38), x = 3479603, y = 400000), size = 4, color = "lightgoldenrod1", family = "Satoshi", fontface = "bold", hjust = 1, vjust = 1) +
-  geom_text(aes(label = str_wrap("Data Center Alley in Northern Virginia has the densest concentration of data centers in the world, with 300+ data centers and 4900 megawatts in energy capacity. Location just outside of Washington, D.C., this area handles, by most estimates, 22-30% of the world's internet traffic.  ", 50), x = 3479603, y = 250000), size = 3, color = "lightgoldenrod3", family = "Satoshi", hjust = 1, vjust = 1) +
+  geom_text(aes(label = str_wrap("Data Center Alley, Northern Virginia", 38), x = 3.5e6, y = 3.7e5), size = 4, color = "orchid2", family = "Satoshi", fontface="bold", hjust = 1, vjust = 1) +
+  geom_text(aes(label = str_wrap("has the densest concentration of data centers in the world, with 300+ data centers and 5000+ megawatts in energy capacity. Located just outside of Washington, D.C., this area handles, by most estimates, 22-30% of the world's internet traffic.  ", 42), x = 3.5e6, y = 2e5), size = 3.5, color = "orchid2", family = "Satoshi", hjust = 1, vjust = 1) +
   labs(
-    title = "A quarter of global internet traffic passes through Virginia's Data Center Alley", hjust = 0.5,
-    subtitle = "Data centers per 1000 square-mile hexagon grids as of November 2025, log-transformed", hjust = 0.5,
-    caption = "#30DayMapChallenge Day 14: OpenStreetMap | Source: OpenSreetMap Contributors, 2025 | Anna Duan X Penn Urban Tech Club",
-    fill = "Data center density\n(Log Centers/100sqmeters)",
+    title = "25% of global internet traffic passes through Virginia's Data Center Alley", 
+    subtitle = "Data centers per 1000 square-mile hex-grid as of November 2025, log-transformed",
+    caption = "#30DayMapChallenge Day 14: OpenStreetMap | Source: OpenStreetMap Contributors, 2025 | Anna Duan X Penn Urban Tech Club",
+    fill = "Data center density\n(Log Centers/1000mi^2"
     
   ) +
   scale_fill_viridis_c(
@@ -46,21 +46,23 @@ ggplot() +
   theme_void() +
   theme(
     legend.position = c(0.1, 0.3),
+    legend.title=element_text(size = 11, face="bold", vjust =1),
+    legend.text =element_text(size=11),
     plot.background = element_rect(fill = "black"),
-    text = element_text(color = "orchid3", family = "Satoshi"),
+    text = element_text(color = "orchid2", family = "Satoshi"),
     plot.title.position = "plot",
     plot.title = element_text(
       face = "bold",
-      size = 23,
-      color = "#FFA400",
+      size = 24,
+      color = "goldenrod1",
       margin = margin(b = 5),
       hjust = 0.5,
     ),
     
     plot.subtitle = element_text(
-      size = 14,
-      hjust = 0,
-      color = "#DE8D00",
+      size = 11,
+      hjust = 0.5,
+      color = "goldenrod2",
       margin = margin(b = 10)
     ),
     
@@ -70,9 +72,8 @@ ggplot() +
       hjust = 0.5,
       margin = margin(t = 10)
     ),
-    legend.title = element_text(face = "bold"),
     plot.margin = margin(t = 20, r = 20, b = 20, l = 20)
   )
 
 #### Group 3: Save the map #### 
-ggsave("map.jpeg", width = 33, height = 20, units = "cm")
+ggsave("newmap.jpeg", width = 30, height = 18, units = "cm")
